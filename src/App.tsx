6a26391f4754cf1a810398fedf8d43db3aefdcbe
Kolade Amire <stephamire@gmail.com>
2024-08-11 10:50:41 -0500

    styled header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 
+import Header from "./Header.tsx";
+
 export default function App() {
 
 
   return (
     <>
-
+      <Header />
     </>
   )
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,22 @@
-interface Props {
+import logoSrc from './assets/logo.svg';
+import './App.css'
 
-}
-
-const Header = (props: Props) => {
-
-}
-
+const Header = () => {
+    return (
+        <header className="header">
+            <div id="logo-container">
+                <img src={logoSrc} id="logo" alt="Logo" />
+            </div>
+            <nav id="header-nav">
+                <ul className="header-nav-list">
+                    <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/">Stories</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/">Music</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/">Login</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/">Sign Up</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
 export default Header
