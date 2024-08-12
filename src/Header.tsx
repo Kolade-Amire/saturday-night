b692c5f089cae7f0b81323cc8c9711bab5a46911
Kolade-Amire <stephamire@gmail.com>
2024-08-12 16:22:53 +0100

    added mobile view

--- src/Header.tsx
+++ src/Header.tsx
@@ -7,12 +7,14 @@ const Header = () => {
             <div id="logo-container">
                 <img src={logoSrc} id="logo" alt="Logo" />
             </div>
-            <nav id="header-nav">
-                <ul className="header-nav-list">
+            <nav id="header-nav" className="header-nav">
+                <ul className="header-nav-list left-nav">
                     <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
                     <li className="nav-item"><a className="nav-link" href="/">Stories</a></li>
                     <li className="nav-item"><a className="nav-link" href="/">Music</a></li>
-                    <li className="nav-item"><a className="nav-link" href="/">Login</a></li>
+                </ul>
+                <ul className="header-nav-list right-nav">
+                    <li className="nav-item" ><a className="nav-link" href="/">Login</a></li>
                     <li className="nav-item"><a className="nav-link" href="/">Sign Up</a></li>
                 </ul>
             </nav>
