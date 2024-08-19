aa76b8ead6113a82338a42685662d45ebbed835a
Kolade Amire <stephamire@gmail.com>
2024-08-19 13:33:56 -0500

    added g logo and removed img

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,4 +1,4 @@
-import homeImgSrc from '../assets/sat-night4.jpg';
+import googleLogoSrc from '../assets/Google__G__logo.svg';
 import '../App.css'
 
 const Body = () => {
@@ -6,16 +6,13 @@ const Body = () => {
         <main className="container mx-auto flex-grow text-center p-4">
 
             <div className="typing-container">
-            <h1 className="typing-animation text-1xl md:text-2xl font-mono font-semibold text-textColorPrimary my-6">
-                Hi, welcome to Saturday Night.
-                Here, you will find the most mind-rumbling, emotion-inducing, unfiltered raw stories about literally anything.
+                <h1 className="text-1xl md:text-2xl font-mono text-wrap font-semibold text-textColorPrimary my-6">
+                    Hi, welcome to Saturday Night.
+                    Here, you will find the most mind-rumbling, emotion-inducing, unfiltered raw stories about literally
+                    anything.
 
-                Join us to dive in! Who knows, you might also have something to share!
-            </h1>
-            </div>
-
-            <div className="faded-edges mt-4">
-                <img src={homeImgSrc} alt="home image" className="max-w-screen max-h-96 w-auto"/>
+                    Join us to dive in! Who knows, you might also have something to share!
+                </h1>
             </div>
             <div className="max-w-md mx-auto bg-primaryColor border-2 border-textFaded p-6 mt-10 rounded-md shadow-md">
                 <h2 className="text-2xl text-textColorPrimary font-bold mb-4">Login / Sign Up</h2>
@@ -32,13 +29,18 @@ const Body = () => {
                             className="w-full bg-secondaryColor text-primaryColor p-2 rounded hover:bg-blue-600">
                         Sign Up
                     </button>
-                    <p className="mt-4 text-gray-600">Have an account? <a href="#"
-                                                                          className="text-secondaryColor font-bold hover:underline hover:text-textFaded ">Login</a>
+                    <p className="mt-4 text-secondaryColor">Have an account?
+                        <a href="#" className="text-secondaryColor font-bold hover:text-textFaded ">
+                            Login
+                        </a>
+                    </p>
+                    <p className="mt-4 text-secondaryColor font-semibold">
+                        Or
                     </p>
                     <div className="mt-4">
                         <button type="button"
-                                className="w-full bg-red-500 text-white p-2 rounded hover:bg-red-600 flex items-center justify-center">
-                            <img src="" alt="Google" className="w-5 h-5 mr-2"/> Sign Up with
+                                className="w-full text-textColorPrimary font-semibold p-2 rounded hover:text-textFaded flex items-center justify-center">
+                            <img src={googleLogoSrc} alt="Google" className="w-5 h-5 mr-2"/> Sign Up with
                             Google
                         </button>
                     </div>
