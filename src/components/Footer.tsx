a4c26567ebf52034452b45061808e22663f5a748
Kolade-Amire <stephamire@gmail.com>
2024-11-22 23:47:45 +0100

    fixed footer color mismatch

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,8 @@
 export default function Footer (props){
     return (
         <footer className={`${props.darkMode ? "dark" : ""} footer-container h-20 text-primaryColor bg-secondaryColor p-4`}>
-            <div className="container  mx-auto text-center">
+            <div className="container mx-auto text-center">
                 <p className="pb-2">Contact us: dev@saturdaynight.com</p>
-
                 <p className="pb-2">All rights reserved © 2024. Saturday Night</p>
             </div>
         </footer>
