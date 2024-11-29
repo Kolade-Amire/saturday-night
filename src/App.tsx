e35b5873cb7411ad2dd8d6a8ff10618755e0b1c3
Kolade-Amire <stephamire@gmail.com>
2024-11-29 20:19:52 +0100

    updated app component for object reference

--- src/App.tsx
+++ src/App.tsx
@@ -23,8 +23,8 @@ export default function App() {
                 <NavBar
                     config ={config}
                 />
-                <Body darkMode={darkMode}/>
-                <Footer darkMode={darkMode}/>
+                <Body darkMode={config.darkMode}/>
+                <Footer darkMode={config.darkMode}/>
             </div>
         </>
     )
